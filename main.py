085df2109fe2ab0512811bea5a134fb64d7684da
WojteGK <93033932+WojteGK@users.noreply.github.com>
2023-11-26 16:54:35 +0100

    added car jsons

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import requests
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import http.client
 import json
-# read file and convert it into a dictionary
-file_directory = 
+import os
+
+def get_file_path():
+    Tk().withdraw() # to hide the main window
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    file_path = askopenfilename(initialdir=current_dir) # show an "Open" dialog box and return the path to the selected file
+    return file_path
+
+    # reszta twojego kodu
 def read_file(file_path):
-    with open('file.json', 'r') as file:
+    with open(file_path, 'r') as file:
         data = json.load(file)
         return data
 
@@ -11,21 +20,38 @@ def generate_car_description(car_data):
     # convert car data to a format that can be understood by the API
     text = f"Brand: {car_data['brand']}\nModel: {car_data['model']}\nYear of production: {car_data['year']}\nColor: {car_data['color']}"
 
+    # create a connection
+    conn = http.client.HTTPSConnection("api.openai.com")
+
     # send a request to the API
-    response = requests.post('https://api.openai.com/v1/engines/davinci-codex/completions', json={
+    payload = json.dumps({
         'prompt': text,
         'max_tokens': 100,
         'temperature': 0.7,
         'n': 1,
         'stop': '\n'
-    }, headers={'Authorization': 'Bearer API_KEY'})
+    })
+    headers = {'Authorization': 'Bearer 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', 'Content-Type': 'application/json'}
+    conn.request("POST", "/v1/engines/davinci-codex/completions", payload, headers)
+
+    # get the response from the API
+    res = conn.getresponse()
+    data = res.read()
 
     # parse the response from the API
-    response_data = json.loads(response.text)
+    response_data = json.loads(data.decode("utf-8"))
 
     # return the generated description
     return response_data['choices'][0]['text'].strip()
+
 def save_car_description(description, file_path):
     # save the description to a file
-    open(file_path, 'w') as file:
-    file.write(description)
+    with open(file_path, 'w') as file:
+        file.write(description)
+
+
+if __name__ == "__main__":
+    file_path = get_file_path()
+    car_data = read_file(file_path)
+    description = generate_car_description(car_data)
+    save_car_description(description, file_path)
